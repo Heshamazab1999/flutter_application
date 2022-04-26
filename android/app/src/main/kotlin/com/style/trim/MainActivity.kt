5794bd68ccdd875c9da281fab5ebdf89a389593e
Heshamazab1999 <60970771+Heshamazab1999@users.noreply.github.com>
2022-04-26 07:55:37 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/style/trim/MainActivity.kt
@@ -0,0 +1,14 @@
+/*
+ * File name: MainActivity.kt
+ * Last modified: 2022.02.13 at 11:18:47
+ * Author: SmarterVision - https://codecanyon.net/user/smartervision
+ * Copyright (c) 2022
+ */
+
+package com.style.trim
+
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
